25a715b708478560cab86c688402cca7102bf7bf
Daniel Davies <daniel@danux.co.uk>
2012-05-11 19:14:36 +0100

    Simple cart

---
+++ cart.py
@@ -0,0 +1,54 @@
+from itertools import imap
+from operator import attrgetter
+
+
+class CartError(Exception):
+	pass
+
+class CartItem(object):
+	def __init__(self, product, quantity=1):
+		self.product = product
+		self.quantity = quantity
+
+	@property
+	def price(self):
+		return self.product.price * self.quantity
+
+class Cart(object):
+	def __init__(self, session):
+		self.cart_items = session.get('cart_items', [])
+
+	def get_cart_item(self, product):
+		try:
+			return filter(lambda x: x.product.product_id == product.product_id, self.cart_items)[0]
+		except IndexError:
+			raise CartError(u'Product is not in cart')
+
+	def add_product(self, product, quantity=1):
+		if filter(lambda cart_item: cart_item.product.product_id == product.product_id, self.cart_items):
+			raise CartError(u'Product is already in cart')
+		self.cart_items.append(CartItem(product, quantity))
+
+	def update_quantity(self, product, quantity):
+		cart_item = self.get_cart_item(product)
+		cart_item.quantity = quantity
+
+	def remove_product(self, product):
+		try:
+			del self.cart_items[[i for i,x in enumerate(self.cart_items) if x.product.product_id == product.product_id][0]]
+		except IndexError:
+			raise CartError(u'Product is not in cart')
+
+	def save(self, session):
+		session['cart_items'] = self.cart_items
+
+	def reset(self):
+		self.cart_items = []
+
+	@property
+	def num_items(self):
+		return sum(imap(attrgetter('quantity'), self.cart_items))
+
+	@property
+	def sub_total(self):
+		return sum(imap(attrgetter('price'), self.cart_items))
---
+++ test.py
@@ -0,0 +1,62 @@
+from cart import Cart, CartError
+import unittest
+
+
+class Product(object):
+    def __init__(self, product_id, price):
+        self.product_id = int(product_id)
+        self.price = price
+
+class TestCart(unittest.TestCase):
+
+	def setUp(self):
+		self.session = {}
+		self.product_one = Product(product_id=1, price=1.99)
+		self.product_two = Product(product_id=2, price=2.49)
+		self.cart = Cart(self.session)
+
+	def test_can_add_product(self):
+		self.cart.add_product(self.product_one, 10)
+		self.assertEqual(self.cart.get_cart_item(self.product_one).quantity, 10)
+
+	def test_cannot_add_product_twice(self):
+		self.cart.add_product(self.product_one, 13)
+		self.assertRaises(CartError, lambda: self.cart.add_product(self.product_one, 10))
+
+	def test_remove_product(self):
+		self.cart.add_product(self.product_one, 10)
+		self.cart.remove_product(self.product_one)
+		self.assertEqual(self.cart.num_items, 0)
+		self.assertRaises(CartError, lambda: self.cart.get_cart_item(self.product_one))
+
+	def test_cannot_remove_product_not_in_cart(self):
+		self.assertRaises(CartError, lambda: self.cart.remove_product(self.product_one))
+
+	def test_can_calculate_quantity_of_cart(self):
+		self.cart.add_product(self.product_one, 10)
+		self.assertEqual(self.cart.num_items, 10)
+		self.cart.add_product(self.product_two, 15)
+		self.assertEqual(self.cart.num_items, 25)
+
+	def test_quantity_of_empty_cart_is_zero(self):
+		self.assertEqual(self.cart.num_items, 0)
+
+	def test_can_calculate_subtotal_of_cart(self):
+		self.cart.add_product(self.product_one, 10)
+		self.assertEqual(self.cart.sub_total, 19.9)
+		self.cart.add_product(self.product_two, 15)
+		self.assertEqual(self.cart.sub_total, 57.25)
+
+	def test_subtotal_of_empty_cart_is_zero(self):
+		self.assertEqual(self.cart.sub_total, 0)
+
+	def can_update_quanity(self):
+		self.cart.add_product(self.product_one, 10)
+		self.cart.update_quantity(self.product_one, 15)
+		self.assertEqual(self.cart.get_cart_item(self.product_one).quantity, 15)
+
+	def test_cannot_update_quantity_if_not_in_cart(self):
+		self.assertRaises(CartError, lambda: self.cart.update_quantity(self.product_one, 10))
+
+if __name__ == '__main__':
+    unittest.main()

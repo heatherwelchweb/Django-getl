43bcf997f17ad34b38e46a9fd26b6e0f557c6cab
Heather Welch <heatherwelch000@outlook.com>
2015-10-28 22:07:39 +0000

    Tryings coveralls with nose

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='shopping_cart',
+    version='0.0.1',
+    packages=[''],
+    url='https://github.com/danux/shopping_cart',
+    license='MIT License',
+    author='Daniel Davies',
+    author_email='danieldavies127@gmail.com',
+    description='Simple Python shopping cart'
+)
